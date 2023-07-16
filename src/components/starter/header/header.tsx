e3ca91118729e4cff8dd67d5a138e46cd893cb60
tanvesh01 <sarvetanvesh01@gmail.com>
2023-07-16 16:47:06 +0530

    fix: main header logo

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -5,7 +5,7 @@ export default component$(() => {
     <>
       <nav class={`${classes.navContainer} p-4 py-3 fixed w-full`}>
         <div class="flex items-center gap-2 justify-center">
-          <img src="/Shundo.svg" width={40} height={40} />
+          <img src="/favicon.svg" width={40} height={40} />
           <p class="font-bold tracking-tighter font-body text-xl">Shundo</p>
         </div>
       </nav>
