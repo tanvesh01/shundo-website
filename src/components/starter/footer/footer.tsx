26b7bd1f6109c1b581f984c16366b29abf5c4bc9
tanvesh01 <sarvetanvesh01@gmail.com>
2023-07-16 16:42:55 +0530

    feat: add og meta tags

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -8,14 +8,43 @@ export default component$(() => {
         <p class={`text-base md:text-xl leading-5  ${styles.offbitStyles}`}>
           <span style="color:#BDBDBD;"> Built while </span> <br />{" "}
           <span style="color:#BDBDBD;">overthinking by </span>
-          <br /> Tanvesh <span style="color:#BDBDBD;"> and </span>Kush
+          <br />
+          <a
+            href="https://twitter.com/Sarve___tanvesh"
+            target="_blank"
+            class="text-white hover:underline"
+          >
+            Tanvesh
+          </a>
+          <span style="color:#BDBDBD;"> and </span>{" "}
+          <a
+            href="https://twitter.com/KushDaga"
+            target="_blank"
+            class="text-white hover:underline"
+          >
+            Kush
+          </a>
         </p>
         <p
           class={`text-base md:text-xl leading-5  ${styles.offbitStyles} `}
           style="color:#BDBDBD;"
         >
-          while standing on top of <br /> incredible projects like Tauri <br />{" "}
-          and windows-rs{" "}
+          while standing on top of <br /> incredible projects like{" "}
+          <a
+            href="https://tauri.app/"
+            target="_blank"
+            class="text-white hover:underline"
+          >
+            Tauri
+          </a>
+          <br /> and{" "}
+          <a
+            href="https://github.com/microsoft/windows-rs"
+            target="_blank"
+            class="text-white hover:underline"
+          >
+            windows-rs
+          </a>
         </p>
       </div>
     </footer>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,19 +3,61 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import HomeHeader from "~/components/landing/HomeHeader";
 
 export default component$(() => {
-  return (
-    <>
-      <HomeHeader />
-    </>
-  );
+  return <HomeHeader />;
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Shundo | a productivity powerhouse for Windows",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content:
+        "shundo is an app for windows that changes the way you do things on windows and how fast you do them. ",
+    },
+    {
+      property: "og:title",
+      content: "Shundo | a productivity powerhouse for Windows",
+      key: "ogtitle",
+    },
+    {
+      property: "og:description",
+      content:
+        "shundo is an app for windows that changes the way you do things on windows and how fast you do them. ",
+      key: "ogdesc",
+    },
+    {
+      property: "og:image",
+      content: "/images/Shundo_og_image.png",
+      key: "ogimage",
+    },
+    {
+      property: "og:url",
+      content: "https://shundo.app",
+      key: "ogurl",
+    },
+    {
+      name: "twitter:image:src",
+      content: "/images/Shundo_og_image.png",
+    },
+    {
+      name: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      name: "twitter:creator",
+      content: "@Sarve___tanvesh",
+      key: "twhandle",
     },
   ],
 };
+
+{
+  /* <meta name="twitter:image:src" content="/meta.png"></meta>
+<meta name="twitter:card" content="summary_large_image"></meta>
+<meta name="twitter:creator" content={twitterHandle} key="twhandle" /> */
+}
+
+// <meta property="og:title" content={"Tanvesh"} key="ogtitle" />
+// <meta property="og:description" content={DESCRIPTION} key="ogdesc" />
+// <meta property="og:image" content={"/meta.png"} key="ogimage" />
+// <meta property="og:url" content={currentURL} key="ogurl" />
