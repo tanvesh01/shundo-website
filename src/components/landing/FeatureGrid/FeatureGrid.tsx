6148fb3f7609096b4bf9c5dcf3c74e4cd6fcaf6f
tanvesh01 <sarvetanvesh01@gmail.com>
2023-10-03 09:07:52 +0530

    fix: padding and other stuff for clipboard container

--- src/components/landing/FeatureGrid/FeatureGrid.tsx
+++ src/components/landing/FeatureGrid/FeatureGrid.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import styles from "./FeatureGrid.module.css";
 export default component$(() => {
   return (
-    <div class="mx-auto border rounded-lg grid grid-cols-8 gap-4 max-w-screen-xl mt-10">
+    <div class="mx-auto rounded-lg grid grid-cols-8 gap-4 max-w-screen-xl mt-10">
       <div class={`p-4 col-span-5 ${styles.green_gradient}`}>
         <h2 class="font-bold tracking-tighter text-2xl text-center">
           File Search
@@ -30,11 +30,11 @@ export default component$(() => {
           faster than the start menu search and gives way better results
         </p>
       </div>
-      <div class={`p-4 col-span-5 ${styles.green_gradient}`}>
-        <h2 class="font-bold tracking-tighter text-2xl text-center">
+      <div class={`p-6 col-span-5 ${styles.green_gradient}`}>
+        <h2 class="font-bold tracking-tighter text-3xl text-center mb-2 ">
           Clipboard
         </h2>
-        <p class="text-center text-gray-300 text-lg max-w-md mx-auto">
+        <p class="text-center text-gray-300 text-lg max-w-md mx-auto mb-6 leading-snug">
           Search through your file system in the quickest way possible, its
           faster than the start menu search and gives way better results
         </p>
